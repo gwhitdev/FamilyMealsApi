f2efdc913dcc517ec9c8913b415ade3bdaf4fc87
Gareth Whitley <me@garethwhitley.online>
2021-01-05 11:34:56 +0000

    update

---
+++ FamilyMealsApi/Models/ReponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace FamilyMealsApi.Models
+{
+    public class ReponseModel
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public List<Ingredient> Data { get; set; }
+    }
+}
